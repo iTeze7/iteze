20af14ee9c7977b495948c1c789604414cc392a9
Higor Endrik <higoroliveira349@gmail.com>
2026-01-08 19:50:09 -0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('year').textContent = new Date().getFullYear();
-
-const badges = document.querySelectorAll('.badge');
-badges.forEach((b, i) => {
-  setTimeout(() => {
-    b.style.transform = 'translateY(-3px)';
-  }, 300 * i);
-});
